e255253e32fa3312e5699f90e516744fb52f099c
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-17 11:27:19 -0600

    CRUD opperators made for the resorce table. I believge I hit MVP - moving on to answering the essay questions

--- resources/resources-router.js
+++ resources/resources-router.js
@@ -7,11 +7,25 @@ const router = express.Router();
 //CRUD actions will go here and will start with /api/resorces
 
 router.get('/', (req, res)=>{
-
+    Resources.find()
+      .then(resources =>{
+          res.status(200).json(resources)
+      })
+      .catch(error=>{
+          res.status(500).json({message: `Server error while trying to pull resources ${error}`})
+      })
 })
 
 router.post('/', (req, res)=>{
+    const resourceData = req.body;
 
+    Resources.add(resourceData)
+      .then(resource =>{
+          res.status(201).json(resource)
+      })
+      .catch(error=>{
+          res.status(500).json({message: `Failed to create new resource due to ${error}`})
+      })
 })
 
 module.exports = router;
