7d8b640f7730ce876c10dea91404f2b3299a4c5f
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-17 10:26:51 -0600

    The Create Project and Read Project operations are working!

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,7 +1,31 @@
 const dB = require('../data/db-config.js')
 
 module.exports = {
+    find,
+    findById,
+    add
     //list of helper functions
 }
 
 //helper functions go here
+
+function find(){
+    return dB('projects')
+}
+
+function findById(id){
+    return dB('projects')
+    .where({ id })
+    .first()
+}
+
+function add(project){
+    return dB('projects')
+    .insert(project, 'id')
+    .then(([id])=>{
+        return findById(id)
+    })
+    .catch(error=>{
+        console.log('Error on add Project', error)
+    })
+}
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -6,5 +6,26 @@ const router = express.Router();
 
 //CRUD actions go here and will start with /api/projects
 
+router.get('/', (req, res)=>{
+    Projects.find()
+    .then(projects =>{
+        res.status(200).json(projects)
+    })
+    .catch(error=>{
+        res.status(500).json({message: `Server error while trying to pull projects ${error}`})
+    })
+})
+
+router.post('/', (req, res)=>{
+    const projectData = req.body;
+
+    Projects.add(projectData)
+      .then(project =>{
+          res.status(200).json(project)
+      })
+      .catch(error =>{
+          res.status(500).json({message: `Failed to create new Project due to ${error}`})
+      })
+})
 
 module.exports = router;
