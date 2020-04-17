b5bc1aaeb30888c388f8fbdc30ab464abb0801be
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-17 09:24:55 -0600

    Skeleton fully built out. Now I'll be designing the layout of the database and moving that arc to a migration

---
+++ projects/projects-model.js
@@ -0,0 +1,7 @@
+const dB = require('../data/db-config.js')
+
+module.exports = {
+    //list of helper functions
+}
+
+//helper functions go here
---
+++ projects/projects-router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const Projects = require('./projects-model.js');
+
+const router = express.Router();
+
+//CRUD actions go here and will start with /api/projects
+
+
+module.exports = router;
